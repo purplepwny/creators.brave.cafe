228bcd4b07b973148c781cde43cdf337ee10f126
Parker J. Banks <parker@pbanks.net>
2021-05-10 14:42:12 -0600

    Add external stylesheet and JS

---
+++ creators.js
@@ -0,0 +1,11 @@
+var urlArray = [
+    "https://brave.cafe/",
+    "https://pbanks.net/",
+    "https://www.guilhermepantoja.com/",
+    "https://carolpelu.com/",
+    "https://recipeasly.org/",
+    "https://ashkankamyab.com/",
+    "https://cubecube.org/",
+    "https://twitter.com/andrei_pestee",
+    "https://3dprintguides.com/"
+];
