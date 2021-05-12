b04e8ce3eba16c6b6c7f3b14834b4aab79a4685f
root <root@pbanks.net>
2021-05-12 19:50:30 +0000

    Updated nav, mobile compatibility, moved scripts to creators.js.

--- creators.js
+++ creators.js
@@ -10,3 +10,9 @@ var urlArray = [
     "https://3dprintguides.com/",
     "https://limbo.center/"
 ];
+
+function randomUrl() {
+    var randomNumber = Math.floor(Math.random() * urlArray.length);
+    var newUrl = urlArray[randomNumber];
+    window.location.href = newUrl;
+}
