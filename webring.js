34d438bf1b6a066dafd5bd2c47cc547d6dffe18b
root <root@pbanks.net>
2021-05-16 06:13:48 +0000

    Changed style, added search, join, embedded site browsing.

---
+++ webring.js
@@ -0,0 +1,50 @@
+var frameArray = [
+    "https://pbanks.net/",
+    "https://www.guilhermepantoja.com/",
+    "https://carolpelu.com/",
+    "https://recipeasly.org/",
+    "https://ashkankamyab.com/",
+    "https://cubecube.org/",
+    "https://3dprintguides.com/",
+    "https://limbo.center/"
+];
+
+const len = frameArray.length;
+var idx = Math.floor(Math.random() * len);
+var modal = document.getElementById("my_modal");
+var webring_button = document.getElementById("show_webring");
+var modal_ring = document.getElementById("my_webring");
+var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+show_slide();
+
+webring_button.onclick = function() {
+    modal.style.display = "block";
+}
+
+function show_slide() {
+    modal_ring.src = frameArray[idx];
+    captionText.innerHTML = frameArray[idx];
+}
+
+function next_slide() {
+    if(idx === (len-1) ) {
+        idx = 0;
+    } else {
+        ++idx;
+    }
+    show_slide();
+}
+
+function prev_slide() {
+    if(idx === 0 ) {
+        idx = len - 1;
+    } else {
+        --idx;
+    }
+    show_slide();
+}
+
+span.onclick = function() { 
+    modal.style.display = "none";
+}
